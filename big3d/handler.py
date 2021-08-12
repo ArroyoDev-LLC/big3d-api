a5b6872baaf311ca1f688b23c97a0c1a420c3699
Braden Mars <bradenmars@bradenmars.me>
2021-08-12 08:39:22 -0500

    feat(big3d): import and setup threedframe config

--- big3d/handler.py
+++ big3d/handler.py
@@ -1,11 +1,19 @@
 import json
+from pathlib import Path
 
 from loguru import logger
+from threedframe.config import config
+
+# Setup threedframe.
+DATA_DIR = Path("/tmp/big3d_api")
+config.RENDERS_DIR = DATA_DIR / "renders"
+config.setup_solid()
 
 
 def hello(event, context):
     logger.info("event: {} {}", event, dir(event))
     logger.info("context: {} {}", context, dir(context))
+    logger.info(config.dict())
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
