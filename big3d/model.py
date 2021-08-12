44a4b72682b72a54a31126e983a3311e4ffdef51
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 13:32:09 -0400

    feat(model): move to single table layout

---
+++ big3d/model.py
@@ -0,0 +1,22 @@
+import uuid
+
+from datetime import datetime
+from pynamodb.model import Model
+from pyanmodb.attribute import UnicodeAttribute, UTCDateTimeAttribute, JSONAttribute
+
+from utils import validate_incoming_data
+
+
+class big3D_table(Base):
+    class Meta:
+        table_name = "dynamodb-big3D"
+        host = "https://localhost:8000"
+
+    order_id = UnicodeAttribute(hash_key=True)
+    user_id = UnicodeAttribute(sort_key=True)
+    created_at = UTCDateTimeAttribute()
+    order_details = JSONAttribute()
+
+    def __iter__(self):
+        for name, attr in self.get_attributes().items():
+            yield name, attr.serialize(getattr(self, name))
