f7b0db7e6572172a8e97153c22a7d356b3f06b99
kmg731 <kyle.goldbeck@gmail.com>
2021-08-11 23:10:27 -0400

    fix(base): make id the hash_key for all child classes

--- big3d/functions/base.py
+++ big3d/functions/base.py
@@ -1,13 +1,14 @@
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 
-
+# Base class to handle common operations
+# Replace the host with the url of the db on aws
 class Base(Model):
     class Meta:
         table_name = "dynamodb-" + cls.__name__
         host = ""
 
-    id = UnicodeAttribute()
+    id = UnicodeAttribute(hash_key=True)
 
     def __iter__(self):
         for name, attr in self.get_attributes().items():
