726d681ae14f6b47aed7a1f66a073815b5b9fd19
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 19:45:27 -0400

    fix(order): fix pathParameters, format scan query correcty

--- big3d/order.py
+++ big3d/order.py
@@ -3,21 +3,13 @@ import json
 
 from datetime import datetime
 
-from big3d.model import big3D_table
-from big3d.utils import validate_incoming_data
+from big3d.model import Big3D
 
 
 def create_new_order(event, context):
-
-    if not event.get("body", None):
-        return {"errorCode": 404, "body": "Body not found"}
-
     data = json.loads(event["body"])
 
-    if not validate_incoming_data(incoming=data, args=["user_id", "order_details"]):
-        return {"errorCode": 422, "body": "Order does not have all required attributes"}
-
-    new_order = big3D_table(
+    new_order = Big3D(
         order_id=str(uuid.uuid1()),
         user_id=data.get("user_id"),
         created_at=datetime.utcnow(),
@@ -26,12 +18,12 @@ def create_new_order(event, context):
 
     new_order.save()
 
-    return {"statusCode": 200, "body": json.dumps(dict(new_order))}
+    return {"statusCode": 201, "body": json.dumps(dict(new_order))}
 
 
-# SSL Validation Error
 def get_user_orders(event, context):
-    # I don't know why event['path'] is only returning a string
-    results = big3D_table.scan(big3D_table.user_id == event["path"][-36])
+    user_id = event["pathParameters"]["user_id"]
+
+    results = Big3D.scan(Big3D.user_id == user_id)
 
     return {"statusCode": 200, "body": json.dumps({"items": [dict(result) for result in results]})}
