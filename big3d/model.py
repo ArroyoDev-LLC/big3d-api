a5010576cfef84fc22038b0b0b5b8de0ba470fe7
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 19:43:45 -0400

    fix(model): change class name, remove broken create table

--- big3d/model.py
+++ big3d/model.py
@@ -4,13 +4,12 @@ from datetime import datetime
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute, JSONAttribute
 
-from big3d.utils import validate_incoming_data
 
-
-class big3D_table(Model):
+class Big3D(Model):
     class Meta:
-        table_name = "dynamodb-big3D"
-        host = "http://localhost:8000"
+        table_name = "dynamodb-Big3D"
+        region = "us-east-1"
+        host = ""
 
     order_id = UnicodeAttribute(hash_key=True)
     user_id = UnicodeAttribute(range_key=True)
@@ -20,6 +19,3 @@ class big3D_table(Model):
     def __iter__(self):
         for name, attr in self.get_attributes().items():
             yield name, attr.serialize(getattr(self, name))
-
-
-big3D_table.create_table(read_capacity_units=1, write_capacity_units=1)
