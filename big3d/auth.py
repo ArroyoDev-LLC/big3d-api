681da5d2cdb6ad8d726a0cbd7a6d06ce53260f4d
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 16:26:40 -0400

    fix(auth): fix, debug, and test routes

--- big3d/auth.py
+++ big3d/auth.py
@@ -1,31 +1,28 @@
 import os
+import json
 
 from pycognito import Cognito
 
-from model import big3D_model
-from big3d.utils import vaildate_incoming_data, validate_email
+from big3d.model import big3D_table
+from big3d.utils import validate_email
 
-USER_POOL_ID = os.environ("USER_POOL_ID")
+USER_POOL_ID = os.environ.get("USER_POOL_ID")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+
+
+def validate_incoming_data(*, incoming, args):
+    for arg in args:
+        if not arg in incoming:
+            return False
+        if not incoming[arg]:
+            return False
+    return True
 
 
 def create_new_user(event, context):
     data = json.loads(event["body"])
 
-    if (
-        found := validate_incoming_data(
-            incoming=data,
-            args=[
-                "given_name",
-                "family_name",
-                "username",
-                "email",
-                "password",
-            ],
-        )
-    ) is not None:
-        return {"error_message": 422, "body": f"User has invalid field {found}"}
-
-    if not validate_email(data["email"]):
+    if not validate_email(email_str=data["email"]):
         return {"error_message": 422, "body": "Invalid email format"}
 
     u = Cognito(USER_POOL_ID)
@@ -39,16 +36,19 @@ def create_new_user(event, context):
 
     u.set_custom_attributes(id=str(uuid.uuid1()))
 
-    u.register(data["username"], data["password"])
+    print(data["username"], data["password"])
+    u.register(data.get("username"), data.get("password"))
 
     u.confirm_sign_up("user_conf_code", username=data["username"])
+
+    return json.dumps(u)
 
 
 # event.headers['id_token']
 
 
 def authenticate_user(event, context):
-    u = Cognito(USER_POOL_ID, email=event.get("email"))
+    u = Cognito(USER_POOL_ID, username=event.get("username"))
 
     u.authenticate(password=event.get("password"))
 
