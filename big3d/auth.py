2a8099c07b2ccd2fdd16a34eaaf613e891a00cc3
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 13:31:13 -0400

    feat(auth): add pycognito user auth functions

---
+++ big3d/auth.py
@@ -0,0 +1,69 @@
+import os
+
+from pycognito import Cognito
+
+from model import big3D_model
+from big3d.utils import vaildate_incoming_data, validate_email
+
+USER_POOL_ID = os.environ("USER_POOL_ID")
+
+
+def validate_email(*, email_str):
+    pattern = r"^[a-z]+[-_$.a-z]*@[a-z]*\.[a-z]+$"
+    match = re.match(pattern, "abc.xyz@gmail.com", re.IGNORECASE)
+
+    return match
+
+
+def create_new_user(event, context):
+    data = json.loads(event["body"])
+
+    if (
+        found := validate_incoming_data(
+            incoming=data,
+            args=[
+                "given_name",
+                "family_name",
+                "username",
+                "email",
+                "password",
+            ],
+        )
+    ) is not None:
+        return {"error_message": 422, "body": f"User has invalid field {found}"}
+
+    if not validate_email(data["email"]):
+        return {"error_message": 422, "body": "Invalid email format"}
+
+    u = Cognito(USER_POOL_ID)
+
+    u.set_base_attributes(
+        email=data["email"],
+        username=data["username"],
+        family_name=data["family_name"],
+        given_name=data["given_name"],
+    )
+
+    u.set_custom_attributes(id=str(uuid.uuid1()))
+
+    u.register(data["username"], data["password"])
+
+    u.confirm_sign_up("user_conf_code", username=data["username"])
+
+
+def authenticate_user(event, context):
+    u = Cognito(USER_POOL_ID, CLIENT_ID, email=event.get("email"))
+
+    u.authenticate(password=event.get("password"))
+
+
+def forgot_password(event, context):
+    u = Cognito(USER_POOL_ID, CLIENT_ID, email=event.get("email"))
+
+    u.initiate_forgot_password()
+
+
+def update_user(event, context):
+    u = Cognito(USER_POOL_ID, CLIENT_ID, event["body"].get("email"))
+
+    u.update_profile(event["body"], arrt_map=dict())
