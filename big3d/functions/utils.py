d9bbe8b067dcbf318c89b81ecf2fdacc27aeb3c4
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 16:25:48 -0400

    fix(functions): remove functions dir, replace in big3d/

--- big3d/functions/utils.py
+++
@@ -1,7 +0,0 @@
-def validate_incoming_data(*, incoming, model_args):
-    for arg in model_args:
-        if not arg in incoming:
-            return False
-        if not incoming[arg]:
-            return False
-    return True
