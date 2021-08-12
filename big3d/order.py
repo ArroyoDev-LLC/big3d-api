59f74f08a5f8f576d5f794bf6970ab8ba6a3e9df
kmg731 <kyle.goldbeck@gmail.com>
2021-08-12 15:13:04 -0400

    fix(order): fix syntax error for arg and and new_order

--- big3d/order.py
+++ big3d/order.py
@@ -1,18 +1,22 @@
 import uuid
+import json
 
 from model import big3D_table
 from big3d.utils import validate_incoming_data
 
+
 def create_new_order(event, context):
-    data = json.loads(event['body'])
+    data = json.loads(event["body"])
 
-    if not validate_incoming_data(incoming=data, args["user_id", "order_details"]):
-        return {"errorCode" : 422, "body" : "Order does not have all required attributes"}
+    if not validate_incoming_data(incoming=data, args=["user_id", "order_details"]):
+        return {"errorCode": 422, "body": "Order does not have all required attributes"}
 
-    new_order = big3D_table(order_id=str(uuid.uuid1()),
-                 user_id=data.get('user_id'),
-                 created_at=datetime.utcnow()
-                 order_details=json.dumps(data['order_details']))
+    new_order = big3D_table(
+        order_id=str(uuid.uuid1()),
+        user_id=data.get("user_id"),
+        created_at=datetime.utcnow(),
+        order_details=json.dumps(data["order_details"]),
+    )
 
     new_order.save()
 
