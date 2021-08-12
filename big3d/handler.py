23bc4ebd8eb4be53a1ade4332bdb4af2ff3589bd
Braden Mars <bradenmars@bradenmars.me>
2021-08-12 09:07:16 -0500

    feat(big3d): add context typing, demo time remaining

--- big3d/handler.py
+++ big3d/handler.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+from awslambdaric.lambda_context import LambdaContext
 from loguru import logger
 from threedframe.config import config
 
@@ -10,13 +11,14 @@ config.RENDERS_DIR = DATA_DIR / "renders"
 config.setup_solid()
 
 
-def hello(event, context):
+def hello(event, context: LambdaContext):
     logger.info("event: {} {}", event, dir(event))
     logger.info("context: {} {}", context, dir(context))
     logger.info(config.dict())
     body = {
         "message": "Go Serverless v2.0! Your function executed successfully!",
         "input": event,
+        "time_remaining": context.get_remaining_time_in_millis(),
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
