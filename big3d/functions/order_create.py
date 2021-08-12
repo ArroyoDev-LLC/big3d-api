047fde9567291fa3aa0d5d78ead6844a7a965556
kmg731 <kyle.goldbeck@gmail.com>
2021-08-11 23:12:45 -0400

    feat(order_create): update new_order with created_at

--- big3d/functions/order_create.py
+++ big3d/functions/order_create.py
@@ -2,6 +2,7 @@ import json
 import uuid
 
 from loguru import logging
+from datetime import datetime
 
 from utils import validate_incoming_data
 from functions.order_model import OrderModel
@@ -10,14 +11,19 @@ from functions.order_model import OrderModel
 def create_order(event, context):
     data = json.loads(event["body"])
 
-    if not validate_incoming_data(incoming=data, model_args=["amount", "user", "details"]):
+    if not validate_incoming_data(
+        incoming=data, model_args=["id", "amount", "user_id", "created_at" "order_details"]
+    ):
         return {
             "statusCode": 422,
             "body": json.dumps({"error_message": "Order does not contain all valid fields"}),
         }
 
     new_order = OrderModel(
-        id=str(uuid.uuid1()), user=data["user_id"], details=json.dumps(data["details"])
+        id=str(uuid.uuid1()),
+        user_id=data["user_id"],
+        created_at=datetime.utcnow(),
+        order_details=json.dumps(data["details"]),
     )
 
     new_order.save()
