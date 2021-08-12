08365f8b2d0df711c41a61e7d9a31bd11cad5a92
kmg731 <kyle.goldbeck@gmail.com>
2021-08-11 23:14:43 -0400

    feat(order): change to allow for getting all orders by user id

--- big3d/functions/order_list.py
+++ big3d/functions/order_list.py
@@ -4,6 +4,7 @@ from functions.order_model import OrderModel
 
 
 def order_list(event, context):
-    results = OrderModel.scan()
+    """Returns all orders for a given user"""
+    results = OrderModel.scan(OrderModel.user == event["path"]["user_id"])
 
     return {"statusCode": 200, "body": json.dumps({"items": [dict(result) for result in results]})}
