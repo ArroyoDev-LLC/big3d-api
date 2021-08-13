4c1e77c70f2374dd90684fe82cb260d3f252d63e
Braden Mars <bradenmars@bradenmars.me>
2021-08-12 20:13:01 -0500

    feat(api): setup mangum/fastapi handler, add basic model compute endpoint

--- big3d/handler.py
+++ big3d/handler.py
@@ -1,8 +1,12 @@
 import json
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from awslambdaric.lambda_context import LambdaContext
+from fastapi import FastAPI, File, UploadFile
 from loguru import logger
+from mangum import Mangum
+from threedframe import utils
 from threedframe.config import config
 
 # Setup threedframe.
@@ -10,17 +14,31 @@ DATA_DIR = Path("/tmp/big3d_api")
 config.RENDERS_DIR = DATA_DIR / "renders"
 config.setup_solid()
 
+app = FastAPI()
 
-def hello(event, context: LambdaContext):
-    logger.info("event: {} {}", event, dir(event))
-    logger.info("context: {} {}", context, dir(context))
-    logger.info(config.dict())
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
-        "time_remaining": context.get_remaining_time_in_millis(),
-    }
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+@app.post("/compute")
+async def compute(model: UploadFile = File(...)):
+    """Compute model properties from uploaded model."""
+    with TemporaryDirectory() as tmpdir:
+        model_path = Path(tmpdir) / "model.blend"
+        model_path.unlink(missing_ok=True)
+        model_path.write_bytes(await model.read())
+        script_path = config.LIB_DIR.parent / "compute.py"
+        assert script_path.exists(), f"Failed to find script path: {script_path}"
+        data_path = model_path.with_suffix(".json")
+        utils.exec_blender_script(Path(model_path), script_path, data_path)
+        logger.info(f"Data written to: {data_path.absolute()}")
+        data = json.loads(data_path.read_text())
+    logger.info("computed results: {}", data)
+    return data
 
+
+def handler(event, context: LambdaContext):
+    """Root handler."""
+    logger.info("event: {}", event)
+    logger.info("context: {}", context)
+    asgi_handler = Mangum(app)
+    response = asgi_handler(event, context)
+    logger.info("response {}", response)
     return response
