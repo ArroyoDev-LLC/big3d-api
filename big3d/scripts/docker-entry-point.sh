75afab1c0565cce159e3df52d6f34e1c1274fe87
Braden Mars <bradenmars@bradenmars.me>
2021-08-13 00:25:26 -0500

    build(docker): add local dev uvicorn serve mode to entrypoint for easy dev

--- big3d/scripts/docker-entry-point.sh
+++ big3d/scripts/docker-entry-point.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  exec /usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric $@
+  if [ -z "${IS_LOCAL}" ]; then
+    exec /usr/local/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric $@
+  else
+    exec /packages/bin/uvicorn --host 0.0.0.0 --port 8080 --reload --reload-dir /function/ handler:app
+  fi
 else
   exec /usr/local/bin/python -m awslambdaric $@
 fi
